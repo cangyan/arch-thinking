5167e144aec6c2795724d5113378e34a5d14ba1e
Mingfeng Wang <me.wmf@foxmail.com>
2019-05-03 17:28:28 +0800

    :sparkles: add urxvt plugin

--- sh/002_font_install.sh
+++ sh/002_font_install.sh
@@ -1,3 +1,10 @@
 wget https://github.com/source-foundry/Hack/releases/download/v3.003/Hack-v3.003-ttf.zip -P /tmp && \
 unzip /tmp/Hack-v3.003-ttf.zip -d /tmp/ && \
+
+
+if [ ! -d "/usr/share/fonts/TTF" ]; then
+    mkdir -p /usr/share/fonts/TTF
+fi && \
+
+
 sudo cp /tmp/ttf/* /usr/share/fonts/TTF && sudo fc-cache -f -v
