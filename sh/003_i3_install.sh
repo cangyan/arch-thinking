db60f1df67fbdbe9a376dea451a88ba3f6f93a6b
Mingfeng Wang <me.wmf@foxmail.com>
2019-05-04 15:49:14 +0800

    :construction: i3block 调整

--- sh/003_i3_install.sh
+++ sh/003_i3_install.sh
@@ -1,10 +1,18 @@
 # 用户角色下运行
 sudo pacman -S i3-gaps i3blocks i3lock i3status rxvt-unicode lua conky compton nitrogen w3m feh --noconfirm && \
 
-# 配置i3
-mkdir -p ~/.config/i3/i3status && sudo cp /etc/i3status.conf ~/.config/i3/i3status/default.conf && \
+sudo pacman -S awesome-terminal-fonts ttf-font-awesome --noconfirm && \
 
-mkdir -p ~/.config/i3/i3blocks && sudo cp /etc/i3blocks.conf ~/.config/i3/i3blocks/default.conf && \
+# 配置i3
+# mkdir -p ~/.config/i3status && sudo cp /etc/i3status.conf ~/.config/i3status/config && \
+
+mkdir -p ~/.config/i3blocks && cp $PWD/../i3/i3blocks.conf ~/.config/i3blocks/config && \
+
+cp $PWD/../i3/battery.sh ~/.config/i3blocks/ && \
+
+cp $PWD/../i3/memory.py ~/.config/i3blocks/ && \
+
+cp $PWD/../i3/volume.sh ~/.config/i3blocks/ && \
 
 cp $PWD/../urxvt/Xresources ~/.Xresources && \
 
