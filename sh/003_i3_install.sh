e50f6f36cc6380ef3e02c7e426b987cdefa5b294
Mingfeng Wang <me.wmf@foxmail.com>
2019-05-04 17:29:34 +0800

    :construction: 调整顺序

--- sh/003_i3_install.sh
+++ sh/003_i3_install.sh
@@ -1,7 +1,7 @@
 # 用户角色下运行
 sudo pacman -S i3-gaps i3blocks i3lock i3status rxvt-unicode lua conky compton nitrogen w3m feh --noconfirm && \
 
-sudo pacman -S awesome-terminal-fonts ttf-font-awesome --noconfirm && \
+sudo pacman -S awesome-terminal-fonts ttf-font-awesome iw --noconfirm && \
 
 # 配置i3
 # mkdir -p ~/.config/i3status && sudo cp /etc/i3status.conf ~/.config/i3status/config && \
